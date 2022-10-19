2d19f7e26117da1d600dff4d30469d3660ea1427
waltonlam <lht20123@gmail.com>
2022-10-19 17:10:05 +0800

    update r script

--- rProject/Openair.R
+++ rProject/Openair.R
@@ -1,5 +1,11 @@
 library(openair)
-
 data("mydata")
+#summaryPlot(mydata, clip = TRUE, percentile = 0.80)
+#summaryPlot(selectByDate(mydata,year = c(2000,2001)),type = "density")
 
-summaryPlot(mydata)
+png(filename = "summaryPlot.png" , width = 500,height = 500)
+summaryPlot(selectByDate(mydata,year = c(2000),month=1),type = "density",
+            avg.time = "hour",period="months",
+            col.trend = "Black",
+            col.data = "green")
+dev.off()
