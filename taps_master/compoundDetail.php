7f9df94bd4f39ed76e809017a0f51f9b1bfb9d41
waltonlam <lht20123@gmail.com>
2022-11-14 12:27:08 +0800

    update compound detail

---
+++ taps_master/compoundDetail.php
@@ -0,0 +1,76 @@
+<?php
+namespace Phppot;
+use Phppot\DataSource;
+require_once "iconn.php";
+require_once "header2.php";
+require_once __DIR__ . '/lib/CompoundModel.php';
+$compoundModel = new CompoundModel();
+if (isset($_POST["submit"])) {
+    //$result = $imageModel->uploadImage();
+    //$id = $imageModel->updateImage($result, $_GET["id"], $_POST['site_code'], $_POST['remark']);
+}
+$result = $compoundModel->selectCompoundById($_GET["id"]);
+$yearFieldBlankAvg = 0;
+
+if(substr($result[0]["sample_id"],5,1) == 'F'){
+	$yearFieldBlankAvg = $compoundModel->calAvgFieldBlank($result[0]["compound_grp"], substr($result[0]["sample_id"],6,2));
+	//echo $yearFieldBlankAvg[0]["avg_ppbv"];
+}
+
+?>
+<html>
+	<head>
+		<link href="assets/style.css" rel="stylesheet" type="text/css" />
+		<style>
+			input[type=button], input[type=submit], input[type=reset] {
+				background-color: #4D9BF3;
+				border: none;
+				color: white;
+				padding: 16px 32px;
+				text-decoration: none;
+				margin: 4px 2px;
+				cursor: pointer;
+			}
+		</style>
+	</head>
+	<body>
+		<div>
+			<h1>Compound Detail</h1>
+			<span id="message" style="color:red"></span>
+			<!--form action="?id=<?php echo $result[0]['id']; ?>" method="post" name="frm-edit" enctype="multipart/form-data"	onsubmit="return imageValidation()"-->
+			<form action="?id=<?php echo $result[0]['id']; ?>" method="post" name="frm-edit" enctype="multipart/form-data">
+				<table>
+					<tr>	
+						<td style="width: 160px; vertical-align: top;">Compound: </td>
+						<td>
+							<input type="text" name="compound" value ="<?php echo $result[0]["compound"]?>" />
+						</td>
+					</tr>
+					<tr>	
+						<td style="width: 160px; vertical-align: top;">Compound Group: </td>
+						<td>
+							<input type="text" name="compound_grp" value="<?php echo $result[0]["compound_grp"]?>" />
+						</td>
+					</tr>	
+
+					<tr>	
+						<td style="width: 160px; vertical-align: top;">Yearly Field Blank Average: </td>
+						<td>
+							<input type="text" name="compound_grp" value="<?php echo $yearFieldBlankAvg[0]["avg_ppbv"]?>" />
+						</td>
+					</tr>
+					
+					<tr>
+						<td></td>
+						<td>
+							<input type="submit" name="submit" value="Submit"> 
+							<input type="button" name="cancel" value="Cancel" onClick="document.location.href='showGlabSample.php'"/>
+						</td>	
+					</tr>
+				</table>	
+			</form>
+		</div>
+		<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+		<script src="assets/validate.js"></script>
+	</body>
+</html>
---
+++ taps_master/lib/CompoundModel.php
@@ -0,0 +1,106 @@
+<?php
+namespace Phppot;
+
+use Phppot\DataSource;
+
+class CompoundModel
+{
+
+    private $conn;
+
+    function __construct()
+    {
+        require_once 'DataSource.php';
+        $this->conn = new DataSource();
+    }
+
+    function getAllImages($start_from, $per_page_record)
+    {
+        $sqlSelect = "SELECT * FROM glab_sample LIMIT $start_from, $per_page_record";
+        $result = $this->conn->select($sqlSelect);
+        return $result;
+    }
+
+    public function insertCompound($imageData, $site_code, $remark)
+    {
+        //$create_date = date('Y-m-d'); 
+        $create_by = $_SESSION['vuserid'];
+        //echo $create_date;
+
+        //print_r($imageData);
+        $query = "INSERT INTO site_photo(name,image,site_code,remark,create_date,last_upd_date,create_by,last_upd_by) VALUES(?,?,?,?,CURDATE(),CURDATE(),?,?)";
+        $paramType = 'ssssss';
+
+        $paramValue = array(
+            $imageData[0],
+            $imageData[1],
+            $site_code,
+            $remark,
+            //"'".$create_date."'",
+            //"'".$create_date."'",
+            $create_by,
+            $create_by
+        );
+
+        $id = $this->conn->insert($query, $paramType, $paramValue);
+        return $id;
+    }
+
+    public function selectCompoundById($id)
+    {
+        $sql = "select * from glab_sample where id=? ";
+        $paramType = 'i';
+        $paramValue = array(
+            $id
+        );
+        $result = $this->conn->select($sql, $paramType, $paramValue);
+        return $result;
+    }
+	
+	public function calAvgFieldBlank($compoundGrp, $year){
+        $sql = "SELECT avg(`conc_ppbv`) as avg_ppbv FROM `glab_sample` WHERE `compound_grp` = ? and `field_blank` = 'Y' and `strt_date` BETWEEN ? and ?";
+        $paramType = 'sss';
+        $paramValue = array(
+            $compoundGrp,
+			"20".$year."-01-01",
+			"20".$year."-12-31"
+        );
+        $result = $this->conn->select($sql, $paramType, $paramValue);
+        return $result;
+    }
+	
+
+    public function updateCompound($imageData, $id, $site_code, $remark)
+    {
+        $query = "UPDATE glab_sample SET name=?, image=?, site_code=?, remark=? WHERE id=?";
+        $paramType = 'ssssi';
+        $paramValue = array(
+            $imageData[0],
+            $imageData[1],
+            $site_code, 
+            $remark,
+            $_GET["id"]
+        );
+        $id = $this->conn->execute($query, $paramType, $paramValue);
+        return $id;
+    }
+
+    /*
+     * public function execute($query, $paramType = "", $paramArray = array())
+     * {
+     * $id = $this->conn->prepare($query);
+     *
+     * if (! empty($paramType) && ! empty($paramArray)) {
+     * $this->bindQueryParams($id, $paramType, $paramArray);
+     * }
+     * $id->execute();
+     * }
+     */
+    function deleteCompoundById($id)
+    {
+        $query = "DELETE FROM glab_sample WHERE id=$id";
+        $result = $this->conn->select($query);
+        return $result;
+    }
+}
+?>
