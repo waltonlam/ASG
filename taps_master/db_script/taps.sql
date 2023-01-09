faa91cd633d93f65ba64af19d93e3666992d0242
waltonlam <lht20123@gmail.com>
2023-01-09 17:10:45 +0800

    update db script

--- taps_master/db_script/taps.sql
+++ taps_master/db_script/taps.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7bd59f5c055298e7c33ee31f1622d3ff07845306af90b643819dc4ae87c8a6b7
-size 3428364
+oid sha256:ebf64370235da79e730fe8ae434c34d0b7166d6a42dca25e535cd5860e6d1bf4
+size 3429045
